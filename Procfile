e7d82e34861ed5bde0ba153765c0612125171c8a
Michael Kirschbaum <mkirsch@clemson.edu>
2016-08-28 09:54:20 -0700

    update requirements

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python app.py
+web: . venv/bin/activate; python app.py
--- tests/flaskr_tests.py
+++ tests/flaskr_tests.py
@@ -1,1 +1,10 @@
-import flaskr
+import os
+import unittest
+import tempfile
+
+class AppTest(unittest.TestCase):
+  def setUp(): pass
+  def tearDown(): pass
+
+if __name__ == '__main__':
+  unittest.main()
