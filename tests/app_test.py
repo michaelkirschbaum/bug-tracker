bb2865b95f59ae85101c75057276ba0a44d0e97b
Michael Kirschbaum <mkirsch@clemson.edu>
2016-08-30 13:07:22 -0700

    create new test

--- tests/app_test.py
+++ tests/app_test.py
@@ -5,14 +5,17 @@ from ..app import app
 
 class AppTest(unittest.TestCase):
 
-  def setUp():
+  def setUp(self):
     self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
     app.config['TESTING'] = True
     self.app = app.test_client()
-    app.init_db()
+    # app.init_db()
 
-  def tearDown():
-    pass
+  def tearDown(self):
+    os.close(self.db_fd)
+    os.unlink(app.config['DATABASE'])
+
+  def test_empty(self): pass
 
 if __name__ == '__main__':
   unittest.main()
