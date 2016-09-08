aacec35fedff8429bd15a79a875632f7a7abbe1c
Michael Kirschbaum <mkirsch@clemson.edu>
2016-09-07 21:45:11 -0700

    add fields to "requests by client" and "request search" buttons

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app.debug = True
 # setup login management
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = "form.html"
 
 from manage import db, Feature, trackerUser
 
